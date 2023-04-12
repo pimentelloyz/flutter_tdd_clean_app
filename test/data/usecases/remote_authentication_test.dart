2acb4560552aaadd2d65a07b3a0aeb4fa776b5a9
André Pimentel <andre.loyz@gmail.com>
2023-04-12 19:46:06 -0400

    test: should return unexpected erro when HttpClient returns 500

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -48,4 +48,13 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+   test("Should throw UnexpectedError if HttpClient returns 500", () async {
+    when(httpClientSpy.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+    .thenThrow(HttpError.serverError);
+
+    final future = sut.auth(params: params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
