8aabf8cd3cacf84de27adea992745724b8d90f3f
André Pimentel <andre.loyz@gmail.com>
2023-04-12 20:14:20 -0400

    test: should return account entity when HttpClient returns 200 with valid data

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,5 +1,6 @@
 import 'package:meta/meta.dart';
 
+import '../../domain/entities/entities.dart';
 import '../../domain/helpers/helpers.dart';
 import '../../domain/usecases/usecases.dart';
 import '../http/http.dart';
@@ -10,10 +11,11 @@ class RemoteAuthentication {
 
   RemoteAuthentication({ @required this.httpClient, @required this.url });
 
-  Future<void> auth({ @required AuthenticationParams params }) async {
+  Future<AccountEntity> auth({ @required AuthenticationParams params }) async {
     final remoteAuthenticationParams = RemoteAuthenticationParams.make(paramsFromDomain: params);
     try {
-      await httpClient.request(url: url, method: 'post', body: remoteAuthenticationParams.toJson());
+      final accountJson = await httpClient.request(url: url, method: 'post', body: remoteAuthenticationParams.toJson());
+      return AccountEntity.fromJson(accountJson);
     } on HttpError catch(error) { 
       throw error == HttpError.unauthorized ? DomainError.invalidCredentials : DomainError.unexpected;
     }
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -22,6 +22,9 @@ void main() {
   });
 
   test("Should call HttpClient with correct values", () async {
+    when(httpClientSpy.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+    .thenAnswer((_) async => { 'accessToken': faker.guid.guid(), 'name': faker.person.name() });
+
     await sut.auth(params: params);
 
     verify(httpClientSpy.request(
@@ -65,5 +68,15 @@ void main() {
     final future = sut.auth(params: params);
 
     expect(future, throwsA(DomainError.invalidCredentials));
+  });
+
+  test("Should return Account entity if HttpClient returns 200", () async {
+    final accessToken = faker.guid.guid();
+    when(httpClientSpy.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+    .thenAnswer((_) async => { 'accessToken': accessToken, 'name': faker.person.name() });
+
+    final account = await sut.auth(params: params);
+
+    expect(account.token, accessToken);
   });
 }
